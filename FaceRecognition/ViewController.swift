ab36f8a73c595e6a217419632051d7efed0f1c44
Ashley Hudson <ah794@kent.ac.uk>
2019-08-13 16:41:32 +0100

    Initial Commit

---
+++ FaceRecognition/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FaceRecognition
+//
+//  Created by Ash on 13/08/2019.
+//  Copyright © 2019 Ash. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
