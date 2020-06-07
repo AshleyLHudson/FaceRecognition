0db93cc57c65068a6bd3e26579342e812ec64f1e
Ashley Hudson <34029213+AshleyLHudson@users.noreply.github.com>
2020-06-07 01:27:31 +0100

    Face “Threat Level” Recognition to Computer Generated Art
    The combination of facial recognition technology and core machine learning through the use of apple’s own technology (Core Image) gives the ability to recognise facial features to determine if someone is “threatening”. The threatening case is done by obtaining a variety of outputs such as if both eyes are showing or if the subject is smiling. The program then will use each of these factors and calculate what the image is deemed to be, such as if each case was “false” then the output would be of a high threatening level. The creative process however is when the program determines what variable is outputted and can generate art through a Plist of stored pieces of art. Demining the art however is a rigorous process as there is quite a lot of research to determine what is “threatening” and how art could be deemed to be “threatening”.

--- FaceRecognition/ViewController.swift
+++ FaceRecognition/ViewController.swift
@@ -12,6 +12,8 @@ import CoreImage
 // Used UInavigation & Imagepicker so that users can pick photos from thier libarary.
 class ViewController: UIViewController, UINavigationControllerDelegate,UIImagePickerControllerDelegate {
     
+    @IBOutlet weak var imageView: UIImageView!
+    
     @IBOutlet weak var currentImage: UIImageView!
     
     @IBOutlet weak var currentText: UITextView!
@@ -33,12 +35,11 @@ class ViewController: UIViewController, UINavigationControllerDelegate,UIImagePi
             // If picked the image, sends it to the image view. (using image alot of times is confusing as hek)
             currentImage.image = image
         }
-        detect()
         // Closes down the image picker
         self.dismiss(animated: true, completion: nil)
     }
     // A function to analyse the image picked
-    func detect()
+    func detect() -> String?
     {
         // Grab the image from the image view
         let imageUsed = CIImage(image: currentImage.image!)!
@@ -59,37 +60,102 @@ class ViewController: UIViewController, UINavigationControllerDelegate,UIImagePi
                 // process all info from faces, displays the
                 let moutShowing = "\n Mouth is showing: \(face.hasMouthPosition)"
                 let isSmiling = "\n Person is smiling: \(face.hasSmile)"
-                var bothEyes = "\n Both eyes showing: True"
+                var bothEyes = "\n Both eyes showing: true"
                 
                 // if statement to catch if both of the eyes are showing
                 if !face.hasRightEyePosition || !face.hasLeftEyePosition
                 {
-                    bothEyes = "\n Both eyes showing: False"
+                    bothEyes = "\n Both eyes showing: false"
                 }
-                // (for fun) test how suspicious the person looks
+                // Test how suspicious the person looks
+                // Start at suspiciousness level 0.
                 var suspectDegree = 0
-                var array = ["Low Thret", "Medium Thret", "High Thret", "Catastrophic Thret" ]
+                // innitallise the array of outputs
+                let array = ["Low Thret", "Medium Thret", "High Thret", "Catastrophic Thret" ]
+                // Get mouth, smile and eyes (with the addition of detection of dace angle.
                 if !face.hasMouthPosition {suspectDegree += 1}
                 if !face.hasSmile {suspectDegree += 1}
                 if bothEyes.contains("false") {suspectDegree += 1}
                 if face.faceAngle > 10 || face.faceAngle < -10 {suspectDegree += 1}
-                let suspectText = "\nSuspeciousness: \(array[suspectDegree])"
                 
-                currentText.text = "\(suspectText) \n\(moutShowing) \(isSmiling) \(bothEyes)"
+                // Output suspiciousness level from array.
+                let suspectText = "\nSuspeciousness: \(array[suspectDegree])"
+                currentText.text = "\(suspectText) \n \(moutShowing) \(isSmiling) \(bothEyes)"
+                return array[suspectDegree]
             }
         }
         else
         {
             currentText.text = "No faces found :("
         }
+        return nil
     }
     
+    
+    // Get a random image when output is low thret
+    func lowThret() {
+       // Find the path for the plist (which allocates the images for the low thret)
+       let path = Bundle.main.path(forResource: "lowthreat", ofType: "plist")
+       let dict = NSDictionary(contentsOfFile: path!)
+    
+       let data = dict?.object(forKey: "Images") as! [String]
+        
+       // Use the image view from main.storyboard and add the randomised image (seen here as random element)
+       imageView.image = UIImage(named: data.randomElement()!)
+    }
+    
+    // Get a random image when output is medium thret
+    func mediumThret() {
+        // Find the path for the plist (which allocate the images for medium thret)
+        let path = Bundle.main.path(forResource: "mediumthreat", ofType: "plist")
+        let dict = NSDictionary(contentsOfFile: path!)
+        
+        let data = dict?.object(forKey: "Images") as! [String]
+        
+        //Use the image view from main.storyboard and add the randomised image (seen here as random element)
+        imageView.image = UIImage(named: data.randomElement()!)
+    }
+    
+    // Get a random image when output is high thret
+    func highThret() {
+        // Find the path for the plist (which allocate the images for high thret)
+        let path = Bundle.main.path(forResource: "highthreat", ofType: "plist")
+        let dict = NSDictionary(contentsOfFile: path!)
+        
+        let data = dict?.object(forKey: "Images") as! [String]
+        
+        //Use the image view from main.storyboard and add the randomised image (seen here as random element)
+        imageView.image = UIImage(named: data.randomElement()!)
+    }
+    
+    // Get a random image when output is catastrophic
+    func catastrophicThret() {
+        // Find the path for the plist (which allocate the images for catastrophic thret)
+        let path = Bundle.main.path(forResource: "cthreat", ofType: "plist")
+        let dict = NSDictionary(contentsOfFile: path!)
+        
+        let data = dict?.object(forKey: "Images") as! [String]
+        
+        //Use the image view from main.storyboard and add the randomised image (seen here as random element)
+        imageView.image = UIImage(named: data.randomElement()!)
+    }
+    // Main function to catch all threats (loading functions) in addition to 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        detect()
-        
-        // Do any additional setup after loading the view.
+        // Call detect function to check if the text detected is either "Low threat, medium threat, high threat or catastrophic threat".
+        let threat = detect()
+        switch threat {
+        case "Low Thret":
+            lowThret()
+        case "Medium Thret":
+            mediumThret()
+        case "High Thret":
+            highThret()
+        case "Catastrophic Thret":
+            catastrophicThret()
+        default:
+            print("no faces found")
+        }
     }
 
 
